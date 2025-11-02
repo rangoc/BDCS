6632576ba7467896daa319fa8ea64e56bf4e9f60
rangoc <cabarkapa.goran@gmail.com>
2025-11-03 00:25:38 +0100

    add a lang attribute

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
 
